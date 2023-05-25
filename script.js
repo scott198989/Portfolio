4607a4e4a895957d18a993a149e5837b45c16952
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-24 20:01:18 -0500

    Trying to add functionality to Next and Previous buttons on projects

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 // Toggle icon nav
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 };
 
 // Scroll section
@@ -13,47 +12,48 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            // Active navbar links
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
-            });
-            // active sections for animation scroll
-            sec.classList.add('show-animate');
-        } else {
-            sec.classList.remove('show-animate');
-        }
+    if (top >= offset && top < offset + height) {
+      // Active navbar links
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
+      });
+      // active sections for animation scroll
+      sec.classList.add('show-animate');
+    } else {
+      sec.classList.remove('show-animate');
+    }
+  });
+
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // Remove toggle icon and navbar when clicking navbar links (scroll)
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menuIcon.classList.remove('bx-x');
+      navbar.classList.remove('active');
     });
+  });
 
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-    
-    // Remove toggle icon and navbar when clicking navbar links (scroll)
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menuIcon.classList.remove('bx-x');
-            navbar.classList.remove('active');
-        });
-    });
-
-    // Animate footer on scroll
-    let footer = document.querySelector('footer');
-    footer.classList.toggle('show-animate', window.innerHeight + window.scrollY >= document.scrollingElement.scrollHeight);
+  // Animate footer on scroll
+  let footer = document.querySelector('footer');
+  footer.classList.toggle('show-animate', window.innerHeight + window.scrollY >= document.scrollingElement.scrollHeight);
 };
+
 // Get the carousel container element
 const carouselContainer = document.querySelector('.carousel-container');
 
 // Get the previous and next buttons
-const prevButton = document.querySelector('.btn-box .btn:first-of-type');
-const nextButton = document.querySelector('.btn-box .btn:last-of-type');
+const prevButton = document.querySelector('.btn.previous-btn:first-of-type');
+const nextButton = document.querySelector('.btn.next-btn:last-of-type');
 
 // Get all the projects in the carousel
 const projects = carouselContainer.querySelectorAll('.project');
@@ -84,4 +84,10 @@ function scrollToCurrentProject() {
     left: projectWidth * currentProjectIndex,
     behavior: 'smooth',
   });
+}
+
+// Function to scroll to a specific section
+function scrollToSection(sectionId) {
+  const section = document.querySelector(`#${sectionId}`);
+  section.scrollIntoView({ behavior: 'smooth' });
 }
